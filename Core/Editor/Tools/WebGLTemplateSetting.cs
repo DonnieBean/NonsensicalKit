e273b2b25e586e25df6cf0c6b99f75a5a85c1c1c
董礼斌 <38479405+DonnieBean@users.noreply.github.com>
2021-11-12 10:35:15 +0800

    Editor文件夹整理

---
+++ Core/Editor/Tools/WebGLTemplateSetting.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class WebGLTemplateSetting : EditorWindow
+{ 
+    /// <summary>
+   /// Build完成后的回调
+   /// </summary>
+   /// <param name="target">打包的目标平台</param>
+   /// <param name="pathToBuiltProject">包体的完整路径</param>
+    [PostProcessBuild(1)]
+    public static void AfterBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        Debug.Log("Build Success  输出平台: " + target + "  输出路径: " + pathToBuiltProject);
+
+        //打开文件或文件夹
+        System.Diagnostics.Process.Start(pathToBuiltProject);
+
+        int index = pathToBuiltProject.LastIndexOf("/");
+
+        Debug.Log("导出包体的目录 :" + pathToBuiltProject.Substring(0, index));
+    }
+    }
