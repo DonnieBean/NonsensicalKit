c7fa295e4f2152976670ec25cc2c961d854c0f70
TonyBean <2479445653@qq.com>
2022-06-29 15:59:15 +0800

    细节优化，少量注释

--- Pool/GameObjectPool.cs
+++ Pool/GameObjectPool.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 namespace NonsensicalKit
 {
+    /// <summary>
+    /// GameObject对象池
+    /// </summary>
     public class GameObjectPool 
     {
-        private GameObject prefab;
-        private Queue<GameObject> queue;
-        private Action<GameObject> resetAction;
-        private Action<GameObject> initAction;
-        private Action<GameObject> firstInitAction;
+        private GameObject prefab;  //预制体
+        private Queue<GameObject> queue;    //待使用的对象
+        private Action<GameObject> resetAction; //返回池中后调用
+        private Action<GameObject> initAction;  //取出时调用
+        private Action<GameObject> firstInitAction; //首次生成时调用
 
         public GameObjectPool( GameObject prefab, Action<GameObject>
             ResetAction , Action<GameObject> InitAction = null, Action<GameObject> FirstInitAction = null)
@@ -22,6 +25,10 @@ namespace NonsensicalKit
             firstInitAction = FirstInitAction;
         }
 
+        /// <summary>
+        /// 取出对象
+        /// </summary>
+        /// <returns></returns>
         public GameObject New()
         {
             if (queue.Count > 0)
@@ -40,6 +47,10 @@ namespace NonsensicalKit
             }
         }
 
+        /// <summary>
+        /// 放回对象
+        /// </summary>
+        /// <param name="obj"></param>
         public void Store(GameObject obj)
         {
             if (queue.Contains(obj)==false)
